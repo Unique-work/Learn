c6c432a91a572739e699840502f145ee7f00ca12
Unique-work <19805062401@163.com>
2021-12-12 00:16:51 +0800

    test

---
+++ src/test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("holll wprd ");
+        System.out.println("holll wprd ");
+        System.out.println("holll wprd ");
+        System.out.println("holll wprd ");
+        System.out.println("holll wprd ");
+        System.out.println("holll wprd ");
+    }
+}
